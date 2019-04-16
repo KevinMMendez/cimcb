46b6ba668b11fc86d71cd4dc60fa24f7ada40db9
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-04-16 10:19:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("README.md", "r") as f:
 
 setup(
     name="cimcb",
-    version="0.0.9",
+    version="0.0.10",
     description="This is a pre-release.",
     long_description=long_description,
     license="http://www.apache.org/licenses/LICENSE-2.0.html",
     url="https://github.com/KevinMMendez/cimcb",
     packages=["cimcb", "cimcb.bootstrap", "cimcb.cross_val", "cimcb.model", "cimcb.plot", "cimcb.utils"],
-    install_requires=["bokeh",
+    python_requires=">=3.5",
+    install_requires=["bokeh>=1.0.0",
                       "keras",
-                      "numpy",
+                      "numpy>=1.12",
                       "pandas",
                       "scipy",
                       "scikit-learn",
@@ -23,4 +24,3 @@ setup(
                       "xlrd"],
     author="Kevin Mendez, David Broadhurst",
     author_email="k.mendez@ecu.edu.au, d.broadhurst@ecu.edu.au",
-)
