3f82b6aadfd0c6eee59ac1392d569f98a12a6a0d
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-04-17 22:05:59 +0800

    Add files via upload

--- cimcb/cross_val/__init__.py
+++ cimcb/cross_val/__init__.py
@@ -1,5 +1,6 @@
 from .kfold import kfold
 from .kfold_average import kfold_average
+from .kfold_parallel import kfold_parallel
 from .holdout import holdout
 
-__all__ = ["kfold", "kfold_average", "holdout"]
+__all__ = ["kfold", "kfold_average", "kfold_parallel", "holdout"]
