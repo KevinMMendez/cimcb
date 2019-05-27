4972f4d33135306d674ba4ba92893061c15a70ce
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-05-27 15:05:34 +0800

    Add files via upload

--- cimcb/utils/load_comparisonXL.py
+++ cimcb/utils/load_comparisonXL.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
 
-def load_comparisonXL(method, evaluate="train"):
+def load_comparisonXL(method, evaluate="train", dropna=True):
     """Load comparison table."""
-    # TO DO: Check that evaluate includes 'test'
-    if evaluate is "test":
+    if evaluate == "test":
         e = "['Test']"
-    elif evaluate is "in bag":
-        e = "['In bag']"
-    elif evaluate is "out of bag":
+    elif evaluate == "in bag":
+        e = "['In Bag']"
+    elif evaluate == "out of bag":
         e = "['Out of Bag']"
     else:
         e = "['Train']"
@@ -27,12 +26,21 @@ def load_comparisonXL(method, evaluate="train"):
     # Remove [ ] from string
     for i in range(len(df)):
         for j in range(len(df.T)):
-            df.iloc[i, j] = df.iloc[i, j][2: -2]
+            if type(df.iloc[i, j]) is str:
+                df.iloc[i, j] = df.iloc[i, j][2: -2]
 
     # Reset index and add methods column
+    method_name = []
+    for i in range(len(method)):
+        name_i = method[i].rsplit('/', 1)[1]
+        method_name.append(name_i)
     df = df.reset_index()
-    df = pd.concat([pd.Series(method, name="method"), df], axis=1, sort=False)
+    df = pd.concat([pd.Series(method_name, name="method"), df], axis=1, sort=False)
     df = df.drop("index", 1)
-    df = df.set_index("method")
+    #df = df.set_index("method")
+
+    # drop columns with just nans
+    if dropna is True:
+        df = df.dropna(axis=1, how='all')
 
     return df
