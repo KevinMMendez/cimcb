f88dd048d163379813f89cd7da1ec3f8f74fb19f
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-06-05 11:13:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="cimcb",
-    version="1.0.17",
+    version="1.0.18",
     description="This is a pre-release.",
     long_description=long_description,
     license="http://www.apache.org/licenses/LICENSE-2.0.html",
