c1bebb2f898dfcd262bb173995b097cee9fe886d
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-08-12 13:30:52 +0800

    Add files via upload

--- cimcb/model/__init__.py
+++ cimcb/model/__init__.py
@@ -1,5 +1,10 @@
 from .NN_LinearSigmoid import NN_LinearSigmoid
 from .NN_SigmoidSigmoid import NN_SigmoidSigmoid
+from .MBNN_LinearSigmoid import MBNN_LinearSigmoid
+from .MBNN_SigmoidSigmoid import MBNN_SigmoidSigmoid
+from .MBNN_LinearSigmoid_1Layer import MBNN_LinearSigmoid_1Layer
+from .MBNN_SigmoidSigmoid_1Layer import MBNN_SigmoidSigmoid_1Layer
+from .NN_SigmoidSigmoidSigmoid import NN_SigmoidSigmoidSigmoid
 from .NN_LinearLogit_Sklearn import NN_LinearLogit_Sklearn
 from .NN_LogitLogit_Sklearn import NN_LogitLogit_Sklearn
 from .PCLR import PCLR
@@ -28,7 +33,7 @@ from .NN_ReluRelu import NN_ReluRelu
 from .RBF_NN import RBF_NN
 
 
-__all__ = ["NN_LinearSigmoid","NN_SigmoidSigmoid","NN_LinearLogit_Sklearn", "NN_LogitLogit_Sklearn", "PCLR", "PCR", "PLS_SIMPLS", "RF", "SVM", "NN_L1", "NN_L2",
+__all__ = ["NN_LinearSigmoid", "NN_SigmoidSigmoid", "MBNN_LinearSigmoid", "MBNN_SigmoidSigmoid", "NN_SigmoidSigmoidSigmoid", "NN_LinearLogit_Sklearn", "NN_LogitLogit_Sklearn", "PCLR", "PCR", "PLS_SIMPLS", "RF", "SVM", "NN_L1", "NN_L2",
            "NN_LinearLinear", "NN_LinearLogit", "NN_LinearTan", "NN_LinearRelu", " NN_LogitLinear", "NN_LogitLogit",
            "NN_LogitTan", "NN_LogitRelu",
            "NN_TanLinear", "NN_TanLogit", "NN_TanTan", "NN_TanRelu",
