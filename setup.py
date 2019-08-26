6ea839bfee305077ad786cc1070cd9500f680b10
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-08-26 14:23:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
   
 setup(
     name="cimcb",
-    version="1.0.37",
+    version="1.0.38",
     description="This is a pre-release.",
     long_description=readme(),
     long_description_content_type='text/markdown',
