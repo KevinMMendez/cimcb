5476be919db9deacf6295fc1a9b7656eeb41cb98
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2019-10-26 11:41:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="cimcb",
-    version="2.0.28",
+    version="2.0.29",
     description="A package containing the necessary tools for the statistical analysis of untargeted and targeted metabolomics data.",
     long_description=readme(),
     long_description_content_type='text/markdown',
